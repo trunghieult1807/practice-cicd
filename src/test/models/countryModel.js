f8bbce4c9ca9655c161dbf4b70f64f103704ba3c
hieult <trunghieult1807@Trungs-MacBook-Pro.local>
2021-07-23 16:57:17 +0700

    fix test

---
+++ src/test/models/countryModel.js
@@ -0,0 +1,20 @@
+import {
+    sequelize,
+    dataTypes,
+    checkModelName,
+    checkPropertyExists,
+  } from "sequelize-test-helpers";
+import chai, { expect } from "chai";
+
+import CountryModel from "../../models/country";
+
+describe("src/models/country", () => {
+    const Country = CountryModel(sequelize, dataTypes);
+    const country = new Country();
+  
+    checkModelName(Country)("Countries");
+  
+    context("properties", () => {
+      ["name", "capital", "population"].forEach(checkPropertyExists(country));
+    });
+  });
